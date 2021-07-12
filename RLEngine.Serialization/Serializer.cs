a859d65610a9bc9b19660f551ea9c55ee46c058b
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2021-07-12 14:04:10 +0200

    [add] serialization basis

---
+++ RLEngine.Serialization/Serializer.cs
@@ -0,0 +1,20 @@
+﻿using RLEngine.Entities;
+
+using System.IO;
+using System.Text.Json;
+
+namespace RLEngine.Serialization
+{
+    public class Serializer
+    {
+        // public static void Main(string[] args)
+        // {
+        //     var options = new JsonSerializerOptions { WriteIndented = true };
+
+        //     var entityType = new EntityType();
+
+        //     var utf8Bytes = JsonSerializer.SerializeToUtf8Bytes(entityType, options);
+        //     File.WriteAllBytes("prueba.json", utf8Bytes);
+        // }
+    }
+}
