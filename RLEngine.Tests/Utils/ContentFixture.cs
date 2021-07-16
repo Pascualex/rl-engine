4c6b22a82a0903a97927cd36693eee4673ffd1ee
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2021-07-16 19:30:50 +0200

    [add] turn manager tests

--- RLEngine.Tests/Utils/ContentFixture.cs
+++ RLEngine.Tests/Utils/ContentFixture.cs
@@ -7,20 +7,34 @@ namespace RLEngine.Tests.Utils
 {
     public class ContentFixture
     {
+        public IEntityType EntityType { get; }
         public IEntityType GroundEntityType { get; }
+        public IEntityType AgentType { get; }
+        public IEntityType SlowAgentType { get; }
+        public IEntityType FastAgentType { get; }
         public IEntityType GhostAgentType { get; }
         public IEntityType UnparentedEntityType { get; }
+        public int StandardActionCost { get; } = 100;
         public ITileType FloorTileType { get; }
         public ITileType WallTileType { get; }
 
         public ContentFixture()
         {
+            EntityType = Substitute.For<IEntityType>();
             GroundEntityType = Substitute.For<IEntityType>();
             GroundEntityType.BlocksGround.Returns(true);
             GhostAgentType = Substitute.For<IEntityType>();
             GhostAgentType.IsAgent.Returns(true);
             GhostAgentType.BlocksGround.Returns(true);
             GhostAgentType.IsGhost.Returns(true);
+            AgentType = Substitute.For<IEntityType>();
+            AgentType.IsAgent.Returns(true);
+            SlowAgentType = Substitute.For<IEntityType>();
+            SlowAgentType.IsAgent.Returns(true);
+            SlowAgentType.Speed.Returns(50);
+            FastAgentType = Substitute.For<IEntityType>();
+            FastAgentType.IsAgent.Returns(true);
+            FastAgentType.Speed.Returns(150);
             UnparentedEntityType = Substitute.For<IEntityType>();
             UnparentedEntityType.Name.Returns("Unparented entity type");
             UnparentedEntityType.IsAgent.Returns(true);
